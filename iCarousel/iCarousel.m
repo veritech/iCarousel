b7dd19922b9225e967cd1a6a22515683336be660
Justin DeVries <sheafify@gmail.com>
2011-08-11 16:44:04 -0500

    Use UIView autoresizing to resize items instead of layoutSubviews
    No consideration for OS X compatibility was made in these changes.

--- iCarousel/iCarousel.m
+++ iCarousel/iCarousel.m
@@ -143,6 +143,7 @@
 	self.itemViews = [NSMutableDictionary dictionary];
     
     contentView = [[UIView alloc] initWithFrame:self.bounds];
+	[contentView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
     
 #ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
     
@@ -527,18 +528,6 @@ NSInteger compareViewDepth(id obj1, id obj2, void *context)
     
 	//hide containers for invisible views
     [view.superview setHidden:([view isHidden] || view.layer.opacity < 0.001)];
-}
-
-#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
-- (void)layoutSubviews
-#else
-- (void)resizeSubviewsWithOldSize:(NSSize)oldSize
-#endif
-{
-    [CATransaction setDisableActions:YES];
-    contentView.frame = self.bounds;
-    [self layOutItemViews];
-    [CATransaction setDisableActions:NO];
 }
 
 - (void)transformItemViews
